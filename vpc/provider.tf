a6dbb2c4427a80cc572822a36ced00ffca7b5b7d
yeswanth <yswnth.2010@gmail.com>
2024-09-24 08:59:43 +0530

    terraform practice

---
+++ vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "sprojex-remote-state"
+    key            = "remote-state-vpc"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
