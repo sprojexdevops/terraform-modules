53606dd1581fcdc69be8b1eb9a7d35ff457b999b
yeswanth <yswnth.2010@gmail.com>
2024-09-26 07:51:31 +0530

    terraform practice

--- local-use/ec2/ec2.tf
+++ local-use/ec2/ec2.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source             = "../modules/ec2"
+  source             = "../../modules/ec2"
   ami_id             = var.ami_id
   instance_type      = "t3.large" #"t3.micro"
   security_group_ids = ["sg-0a1fb132f1fc1e49d"]
--- local-use/vpc/vpc.tf
+++ local-use/vpc/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                = "../modules/vpc"
+  source                = "../../modules/vpc"
   project_name          = var.project_name
   environment           = var.environment
   vpc_cidr              = var.vpc_cidr
